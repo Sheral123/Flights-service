af4aa2724f0dc9ffe2caae7304277b9d5f44869b
Sheral123 <sheralsingla18@gmail.com>
2024-06-05 16:48:00 +0530

    added complex join operation for Airport and Airplane details for flight api

--- src/repositories/flight-repo.js
+++ src/repositories/flight-repo.js
@@ -1,5 +1,7 @@
+const {Sequelize, Op} = require('sequelize')
 const CrudRepo = require('./crud-repo');
-const { Flight } = require('../models')
+
+const { Flight, Airplane, Airport } = require('../models')
 class FlightRepo extends CrudRepo{
     constructor(){
         super(Flight);
@@ -8,7 +10,33 @@ class FlightRepo extends CrudRepo{
     async getAllFlights(filter,sort){
         const response = await Flight.findAll({
             where: filter,
-            order: sort
+            order: sort,
+            include:[
+                {
+                    model: Airplane,
+                    required: true,
+                    as: 'airplaneDetail' 
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    }
+                    
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    }
+                    
+                },                
+                      
+            ]        
         });
         return response;
     }
